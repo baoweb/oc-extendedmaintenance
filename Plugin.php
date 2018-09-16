01e7811d6b328380eedad2e0c973cd2ccd3b3b2d
Martin Kryl <martin@baoweb.cz>
2018-09-16 10:27:52 +0200

    Cleaning up the plugin file

--- Plugin.php
+++ Plugin.php
@@ -22,21 +22,11 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-            'name'        => 'ExtendedMaintenance',
-            'description' => 'No description provided yet...',
-            'author'      => 'Baoweb',
-            'icon'        => 'icon-leaf'
+            'name' => 'ExtendedMaintenance',
+            'description' => 'Plugin that allows access only to superadmins.',
+            'author' => 'Baoweb',
+            'icon' => 'icon-leaf'
         ];
-    }
-
-    /**
-     * Register method, called when the plugin is first registered.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
     }
 
     /**
@@ -51,13 +41,13 @@ class Plugin extends PluginBase
 
             $isInMaintenanceMode = Config::get('baoweb.extendedmaintenance::maintenance_mode');
 
-            if(!$isInMaintenanceMode) {
+            if (!$isInMaintenanceMode) {
                 return true;
             }
 
             $user = BackendAuth::getUser();
 
-            if($user && !$user->is_superuser) {
+            if ($user && !$user->is_superuser) {
 
                 $template = File::get(__DIR__ . '/templates/closed.htm');
 
@@ -65,60 +55,6 @@ class Plugin extends PluginBase
 
                 die();
             }
-
-
         });
-
-    }
-
-    /**
-     * Registers any front-end components implemented in this plugin.
-     *
-     * @return array
-     */
-    public function registerComponents()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'Baoweb\ExtendedMaintenance\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * Registers any back-end permissions used by this plugin.
-     *
-     * @return array
-     */
-    public function registerPermissions()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'baoweb.extendedmaintenance.some_permission' => [
-                'tab' => 'ExtendedMaintenance',
-                'label' => 'Some permission'
-            ],
-        ];
-    }
-
-    /**
-     * Registers back-end navigation items for this plugin.
-     *
-     * @return array
-     */
-    public function registerNavigation()
-    {
-        return []; // Remove this line to activate
-
-        return [
-            'extendedmaintenance' => [
-                'label'       => 'ExtendedMaintenance',
-                'url'         => Backend::url('baoweb/extendedmaintenance/mycontroller'),
-                'icon'        => 'icon-leaf',
-                'permissions' => ['baoweb.extendedmaintenance.*'],
-                'order'       => 500,
-            ],
-        ];
     }
 }
